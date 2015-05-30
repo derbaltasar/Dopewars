d58d31476f58188b2a9705774761dd6045b93570
Philipp Jess <philippjess@gmail.com>
2015-05-30 21:50:38 +0200

    made tables work

--- src/com/ideasengineers/MainFXMLController.java
+++ src/com/ideasengineers/MainFXMLController.java
@@ -60,6 +60,8 @@ public class MainFXMLController implements Initializable {
             b.generateNewValue();
         }
         
+        regionField.setText(activePlayer.getActiveRegion().getName());
+        bankField.setText(String.valueOf(activePlayer.getCash()));
         
     }
     
@@ -84,7 +86,10 @@ public class MainFXMLController implements Initializable {
         marketTable.setItems(activePlayer.getActiveRegion().getDrugs());
         namePocketRow.setCellValueFactory((CellDataFeatures<Drug, String> param) -> param.getValue().getName());
         countRow.setCellValueFactory((CellDataFeatures<Drug, String> param) -> new SimpleStringProperty(String.valueOf(param.getValue().getCount())));
-        marketTable.setItems(activePlayer.getDrugPocket());
+        pocketTable.setItems(activePlayer.getDrugPocket());
+        
+        regionField.setText(activePlayer.getActiveRegion().getName());
+        bankField.setText(String.valueOf(activePlayer.getCash()));
     }
     
     private void initRegions() {
